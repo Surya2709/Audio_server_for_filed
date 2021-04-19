fd0a0dd97c634af0cb1d1dfb022b68aadd71fc92
Surya2709 <suryaramachandran27@gmail.com>
2021-04-19 13:14:11 +0530

    modified:   main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 
 from flask import Flask, render_template, redirect
-import os
+from flask import Blueprint, request, jsonify
+
 
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    return "you are up"
+blueprint = Blueprint("main", __name__, url_prefix="/")
 
+
+@blueprint.route("/")
+def home():
+    return "We are live !"
 
 if __name__ == "__main__":
+    app.register_blueprint(blueprint)
     app.run(debug=True)
