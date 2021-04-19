07fae259738829c4b10ab22c9849c614413aa944
Surya2709 <suryaramachandran27@gmail.com>
2021-04-19 13:56:14 +0530

    modified:   database.py

--- database.py
+++ database.py
@@ -7,3 +7,24 @@ migrate = Migrate()
 Column = database.Column
 
 relation = database.relationship
+
+
+
+class CRUDMIXIN(object):
+    def create():
+        pass
+    def update():
+        pass
+    def save():
+        pass
+    def delete():
+        pass
+    
+def Model(CRUDMIXIN , database.Model):
+    __abstract__ = True
+
+class SurrogatePK(object):
+    @classmethod
+    def get_by_id():
+        pass
+    
