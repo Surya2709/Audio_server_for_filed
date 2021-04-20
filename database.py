3ac446f935eabff636804e1172532ea4ae33007a
Surya2709 <suryaramachandran27@gmail.com>
2021-04-20 13:46:51 +0530

    deleted:    audio_files.py 	modified:   main.py

--- database.py
+++ database.py
@@ -1,30 +1,37 @@
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
 
-database = SQLAlchemy()
-migrate = Migrate()
+from flask import Flask
+import pymongo
 
-Column = database.Column
+import datetime
 
-relation = database.relationship
+mongo = pymongo.MongoClient("localhost",port=27017,serverSelectionTimeoutMS=1000)
+db =mongo
+mongo.server_info()
 
 
+class AudioBook(db.Song):
+    __tablename__ = "audiobook"
+    uploaded_time = Column(
+        db.DateTime, default=db.func.now(), nullable=False, onupdate=datetime.utcnow()
+    )
+    duration_time = Column(db.Integer, default=0, nullable=False)
+    title = Column(db.String(100), nullable=False)
+    author = Column(db.String(100), nullable=False)
+    narrator = Column(db.String(100), nullable=False)
 
-class CRUDMIXIN(object):
-    def create():
-        pass
-    def update():
-        pass
-    def save():
-        pass
-    def delete():
-        pass
-    
-def Model(CRUDMIXIN , database.Model):
-    __abstract__ = True
+class Podcast():
+    uploaded_time = Column(
+        db.DateTime, default=db.func.now(), nullable=False, onupdate=datetime.utcnow()
+    )
+    duration_time = Column(db.Integer, default=0, nullable=False)
+    __tablename__ = "podcast"
+    name = Column(db.String(100), nullable=False)
+    host = Column(db.String(100), nullable=False)
+    participents = Column(db.Text, nullable=False)
 
-class SurrogatePK(object):
-    @classmethod
-    def get_by_id():
-        pass
+
+class Song(db.audioserver.Song):
+    name = 
+    uploaded_time = 
+    duration_time =
     
--- main.py
+++ main.py
@@ -1,40 +1,115 @@
-
+from flask import Flask
+import pymongo
 from flask import Flask, render_template, redirect
 from flask import Blueprint, request, jsonify
-
-
-
-
+import datetime
 app = Flask(__name__)
 
 
-blueprint = Blueprint("main", __name__, url_prefix="/")
+try:
+    mongo = pymongo.MongoClient("localhost",port=27017,serverSelectionTimeoutMS=1000)
+
+    mongo.server_info()
+
+except:
+    print( " ERROR - can't connect to db")
 
 
-@blueprint.route("/")
-def home():
-    return "We are live !"\
-
-
-@blueprint.route("/create")
+@app.route("/create", methods=["POST"])
 def create_api():
-    return "create API"
 
-@blueprint.route("/update")
-def update_api():
-    return "update API"
+    if request.method == "POST":
 
-@blueprint.route("/delete")
-def delete_api():
-    return "delete API"
 
-@blueprint.route("/get")
-def get_api():
-    return "get API"
+        data = request.json
+        type = data.get("audioFileType", None)
+
+        if type is None:
+            return "The request is invalid: 400 bad request", 400
+        metadata = data.get("audioFileMetadata")
+        if type == "song":
+
+            try:
+                
+                name = metadata["name"]
+                # check duration time and upload time
+                if int(metadata["duration_time"]) <= 0:
+                    metadata["duration_time"] = 0
+                duaration = metadata["duration_time"]
+                if len(name)>100 :
+                    return "The request is invalid: 400 bad request", 400
+                
+            except:
+                 return "The request is invalid: 400 bad request", 400
+
+
+            uploaded_time  = datetime.datetime.utcnow()
+
+            id = mongo.audioserver.Song.count()
+            id = id+1
+
+            try:
+                mongo.audioserver.Song.insert({'ID':id,'Name': name, 'Duration': duaration,'Uploaded_time': uploaded_time})
+                return "200 ok", 200
+            except:
+                return "The request is invalid: 400 bad request", 400
+
+
+        if type == "podcast":
+            
+            participant_ = ""
+            try:
+                participants = metadata.get("participants", None)
+            except:
+                participant_ = ""
+
+
+            if participants is None:
+                participant_ = ""
+            else:
+                if (
+                    len(participants) > 10
+                    or any(i for i in participants if len(i) > 100)
+                ):
+                    return "The request is invalid: 400 bad request", 400
+
+                participant_ = ",".join(participants)
+            
+                try:
+                    host = metadata['host']
+                    if int(metadata["duration_time"]) <= 0:
+                        metadata["duration_time"] = 0
+                    duaration = metadata["duration_time"]
+                    if len(host)>100 :
+                        return "The request is invalid: 400 bad request", 400
+                    uploaded_time  = datetime.datetime.utcnow()
+
+                    id = mongo.audioserver.Podcast.count()
+                    id = id+1
+                except:
+                    return "The request is invalid: 400 bad request", 400
+                try:
+                    try:
+                        mongo.audioserver.Podcast.insert({'ID':id,'Name': name, 'Duration': duaration,'Uploaded_time': uploaded_time})
+                        return "200 ok", 200
+                    except:
+                        return "The request is invalid: 400 bad request", 400
+                except:
+                    return "The request is invalid: 400 bad request", 400
+
+                            
+                    
+
+       
+    return "The request is invalid: 400 bad request", 400
+
+            
+
+
+
 
 
 
 
 if __name__ == "__main__":
-    app.register_blueprint(blueprint)
     app.run(debug=True)
