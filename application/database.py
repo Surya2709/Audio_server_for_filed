938dd924d2445da861379907b98a72ed68d7be94
Surya2709 <suryaramachandran27@gmail.com>
2021-04-20 14:41:10 +0530

    new file:   application/__init__.py 	new file:   application/creator.py 	new file:   application/database.py 	modified:   main.py

---
+++ application/database.py
@@ -0,0 +1,10 @@
+import pymongo
+
+
+try:
+    mongo = pymongo.MongoClient("localhost",port=27017,serverSelectionTimeoutMS=1000)
+
+    mongo.server_info()
+
+except:
+    print( " ERROR - can't connect to db")
